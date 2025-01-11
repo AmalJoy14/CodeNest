095fe3276e7e8fc234482c52c1ed8a7dd523fe20
AmalJoy14 <amaljoy246@gmail.com>
2025-01-11 22:32:22 +0530

    Modified

---
+++ src/components/intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Intro.css';
+import logo from '../assets/w-logo.svg'; 
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <img src={logo} alt="CodeNest Logo" className="intro-logo" />
+      <h1 className="intro-title">
+        <span className="animate-letter">C</span>
+        <span className="animate-letter">o</span>
+        <span className="animate-letter">d</span>
+        <span className="animate-letter">e</span>
+        <span className="animate-letter">N</span>
+        <span className="animate-letter">e</span>
+        <span className="animate-letter">s</span>
+        <span className="animate-letter">t</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Intro;
