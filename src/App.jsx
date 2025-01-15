bb55be918db3f0f9adaf991c5518a61097c08de5
anushsk2004 <anushsk2004@gmail.com>
2025-01-15 22:10:09 +0530

    Created Signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Intro from './components/intro';
+import SignUp from './components/signup';
+import SignIn from './components/signin';
 
 const App = () => {
   return (
-    <div className="App">
-      <Intro/>
-    </div>
+    <Router>
+      <Routes>
+        {/* Intro page will automatically transition to SignUp after 5 seconds */}
+        <Route path="/" element={<IntroWithTimer />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
+};
+
+
+const IntroWithTimer = () => {
+  const navigate = useNavigate(); 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/signup'); 
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  }, [navigate]);
+
+  return <Intro />;
 };
 
 export default App;
