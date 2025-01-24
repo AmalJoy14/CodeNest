9ac21076486895589c493cda710fe00899f2151d
AmalJoy14 <amaljoy246@gmail.com>
2025-01-24 23:54:23 +0530

    added platform logos

--- frontend/src/components/Leaderboard/leaderboard.jsx
+++ frontend/src/components/Leaderboard/leaderboard.jsx
@@ -1,13 +1,35 @@
 import  Header  from '../header.jsx'
 import  Footer  from '../footer.jsx'
 import React, { useState } from "react"
+import UsernameForms from "./UsernameForms"
 import LeaderboardTable from "./leaderboardTable.jsx"
 import styles from "./leaderboard.module.css"
 
+import leetcodeLogo from "../../assets/leetcode_logo.png"
+import codeforcesLogo from "../../assets/codeforces_logo.png"
+import codenestLogo from "../../assets/b-logo.svg"
+
 const platforms = ["LeetCode", "Codeforces", "CodeNest"]
+const logoMap = {
+  LeetCode: leetcodeLogo,
+  Codeforces: codeforcesLogo,
+  CodeNest: codenestLogo,
+}
 
 export default function LeaderboardPage() {
   const [activeTab, setActiveTab] = useState("LeetCode")
+  const [usernames, setUsernames] = useState({
+    LeetCode: "",
+    Codeforces: "",
+    CodeNest: "",
+  })
+
+  const handleUsernameChange = (platform, username) => {
+    setUsernames((prevUsernames) => ({
+      ...prevUsernames,
+      [platform]: username,
+    }))
+  }
 
   return (
     <div className={styles.container}>
@@ -21,11 +43,16 @@ export default function LeaderboardPage() {
               className={`${styles.tab} ${activeTab === platform ? styles.activeTab : ""}`}
               onClick={() => setActiveTab(platform)}
             >
+              <div className={styles.iconbox} data-platform={platform}>
+                <img src={logoMap[platform]} className={styles.icon} />
+              </div>
+
               {platform}
             </button>
           ))}
         
       </div>
+      <UsernameForms platform={activeTab} username={usernames[activeTab]} onUsernameChange={handleUsernameChange} />
       <LeaderboardTable platform={activeTab} />
       <Footer />
     </div>
