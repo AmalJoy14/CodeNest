0178206e437719cf86df60ca9edd900c1656d63e
AmalJoy14 <amaljoy246@gmail.com>
2025-03-23 00:18:12 +0530

    refactor Vite config to set external dependencies and adjust chunk size warning limit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
+      external: ['react', 'react-dom'],
       output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
             if (id.includes('codemirror')) return 'codemirror-vendor';
             return 'vendor';
           }
-        },
-      },
+        }
+      }
     },
+    chunkSizeWarningLimit: 700,
   },
   server: {
     port: 5173,
-  },
+  }
 });
