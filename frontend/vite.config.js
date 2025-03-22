58022336dad2ccc69972c1bc3922fa9744fef359
AmalJoy14 <amaljoy246@gmail.com>
2025-03-23 00:21:28 +0530

    update Vite config to increase chunk size warning limit to 900

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,22 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        },
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('codemirror')) return 'codemirror-vendor';
-            return 'vendor';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 700,
+    chunkSizeWarningLimit: 900,
   },
   server: {
     port: 5173,
