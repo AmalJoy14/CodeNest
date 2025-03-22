9aacb05b60b268c775e4ed6d6fdbe38225439c61
AmalJoy14 <amaljoy246@gmail.com>
2025-03-22 19:53:31 +0530

    refactor code editor to load language extensions using a modular approach

--- frontend/src/components/Problems/codeEditor.jsx
+++ frontend/src/components/Problems/codeEditor.jsx
@@ -1,36 +1,22 @@
 import React, { useState, useEffect } from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { material } from "@uiw/codemirror-theme-material";
 
 const CodeEditor = ({ code, onCodeChange, language }) => {
-  const [langExtension, setLangExtension] = useState(null);
+  const [langExtension, setLangExtension] = useState([]);
 
   useEffect(() => {
     const loadLanguage = async () => {
-      let extension;
-      switch (language) {
-        case "javascript":
-          extension = (await import("@codemirror/lang-javascript")).javascript();
-          break;
-        case "python":
-          extension = (await import("@codemirror/lang-python")).python();
-          break;
-        case "cpp":
-          extension = (await import("@codemirror/lang-cpp")).cpp();
-          break;
-        case "java":
-          extension = (await import("@codemirror/lang-java")).java();
-          break;
-        case "go":
-          extension = (await import("@codemirror/lang-go")).go();
-          break;
-        case "rust":
-          extension = (await import("@codemirror/lang-rust")).rust();
-          break;
-        default:
-          extension = (await import("@codemirror/lang-javascript")).javascript();
-      }
-      setLangExtension(extension);
+      const langModules = {
+        javascript: () => import("@codemirror/lang-javascript").then(m => m.javascript()),
+        python: () => import("@codemirror/lang-python").then(m => m.python()),
+        cpp: () => import("@codemirror/lang-cpp").then(m => m.cpp()),
+        java: () => import("@codemirror/lang-java").then(m => m.java()),
+        go: () => import("@codemirror/lang-go").then(m => m.go()),
+        rust: () => import("@codemirror/lang-rust").then(m => m.rust()),
+      };
+
+      const extension = langModules[language] ? await langModules[language]() : await langModules["javascript"]();
+      setLangExtension([extension]);
     };
 
     loadLanguage();
@@ -41,8 +27,7 @@ const CodeEditor = ({ code, onCodeChange, language }) => {
       value={code}
       height="100%"
       width="100%"
-      theme={material}
-      extensions={langExtension ? [langExtension] : []}
+      extensions={langExtension}
       onChange={(value) => onCodeChange(value)}
       basicSetup={{ foldGutter: false }}
     />
