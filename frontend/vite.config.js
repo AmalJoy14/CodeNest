6b02e619652344813a18b8930af5e10ed4fd9b5f
AmalJoy14 <amaljoy246@gmail.com>
2025-03-27 19:15:34 +0530

    add jsconfig.json for TypeScript support; update Vite config to remove commonjs plugin; modify package.json for cookie dependency and change module type

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 
 
 export default defineConfig({
-  plugins: [react() , commonjs()],
+  plugins: [react() ],
   build: {
     chunkSizeWarningLimit: 900,
   },
